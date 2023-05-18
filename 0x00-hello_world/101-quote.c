0286b7876c5768860465687ab7eb490bbfe34dfc
Maryelh <elhammouchi.maryam@gmail.com>
2023-05-18 02:54:21 -0700

    UNIX as operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - C program that prints a new line to the standard error
+ * main - Entry point
  *
- * Return: 1 for success
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
