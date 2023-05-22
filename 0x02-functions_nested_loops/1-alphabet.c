ef4c848f910fd57c53bc6104d5f07680c3aa34cb
Maryelh <elhammouchi.maryam@gmail.com>
2023-05-22 13:05:12 -0700

    1.insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
