8a70445486ef17a312830846c174e3e728ecfac2
Maryelh <elhammouchi.maryam@gmail.com>
2023-05-22 13:01:02 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * main - A program that prints _putchar
+ * _putchar: printing the alphabet putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "_putchar\n";
+
+	write(1, str, 9);
+	return (0);
+}
